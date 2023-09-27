ca1d4f8b68f291a11b7a6bb67ce1e035220ba8be
Russell Emerine <rre0520@gmail.com>
2023-09-27 14:57:42 -0400

    Convert .tff to .pnct rectangles and .txtr textures

--- freetype-test.cpp
+++ freetype-test.cpp
@@ -1,9 +1,6 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
-#include <hb.h>
-#include <hb-ft.h>
-
 #include <iostream>
 #include <fstream>
 #include <cassert>
@@ -13,7 +10,7 @@
 #include "read_write_chunk.hpp"
 
 // TODO: put constants in a common file
-constexpr size_t PIXEL_COUNT = 36;
+constexpr size_t PIXEL_COUNT = 72;
 constexpr float PIXEL_SCALE = 0.1;
 
 int main(int argc, char **argv) {
@@ -22,7 +19,7 @@ int main(int argc, char **argv) {
     
     if (FT_Init_FreeType(&ft_library)) {
         assert(false && "Problem initializing FreeType");
-    };
+    }
     // TODO: copy fonts into dist
     if (FT_New_Face(ft_library,
                     data_path("fonts/Inknut_Antiqua/InknutAntiqua-Regular.ttf").c_str(),
@@ -47,16 +44,12 @@ int main(int argc, char **argv) {
         glm::u8vec4 Color;
         glm::vec2 TexCoord;
         
-        /*
-         * Constructs a very plain very boring vertex
-         *
-         * (Not even sure what TexCoord is exactly but I'll assume position is fine there)
-         */
-        explicit Vertex(glm::vec3 position) :
+        // Constructs a very plain very boring vertex
+        explicit Vertex(glm::vec3 position, glm::vec2 tex_coord) :
                 Position(position),
                 Normal(0.0, 0.0, 1.0),
                 Color(0x00, 0x00, 0x00, 0xff),
-                TexCoord(position) {}
+                TexCoord(tex_coord) {}
     };
     static_assert(sizeof(Vertex) == 3 * 4 + 3 * 4 + 4 * 1 + 2 * 4, "Vertex is packed.");
     
@@ -64,105 +57,148 @@ int main(int argc, char **argv) {
         uint32_t name_begin, name_end;
         uint32_t vertex_begin, vertex_end;
     };
-    static_assert(sizeof(IndexEntry) == 16, "Index entry should be packed");
+    static_assert(sizeof(IndexEntry) == 16, "IndexEntry should be packed");
     
-    std::vector<Vertex> data;
+    struct TexIndexEntry {
+        uint32_t name_begin, name_end;
+        uint32_t tex_begin, tex_end;
+        uint32_t width, height;
+    };
+    static_assert(sizeof(TexIndexEntry) == 24, "TexIndexEntry should be packed");
+    
+    std::vector<Vertex> vertices;
+    std::vector<uint8_t> texture_colors;
     std::vector<char> strings;
-    std::vector<IndexEntry> index;
+    std::vector<IndexEntry> vertex_indices;
+    std::vector<TexIndexEntry> texture_indices;
     
     size_t glyph_count = 0;
-    // Loop through all the glyphs and render them in .pnct format.
+    // Loop through all the glyphs and render them in .pnct and .txtr formats.
     // I couldn't find what glyph indices are officially valid, but it seems like this works for the font I'm using.
     for (FT_Long glyph_index = 0; glyph_index < face->num_glyphs; glyph_index++) {
+        IndexEntry index_entry{};
+        TexIndexEntry tex_index_entry{};
+        { // Find the name and add it
+            /*
+             * There's probably not an official limit anywhere since anyone can make a font, but
+             * - https://github.com/adobe-type-tools/agl-specification
+             * - https://learn.microsoft.com/en-us/typography/opentype/spec/post
+             * - http://adobe-type-tools.github.io/afdko/OpenTypeFeatureFileSpecification.html#2fi-glyph-name
+             * agree that it's maximum 63 characters. So, 64 for the 63 and a null terminator.
+             */
+            char buf[64];
+            /*
+             * From the FreeType documentation https://freetype.org/freetype2/docs/reference/ft2-information_retrieval.html#ft_get_glyph_name:
+             *
+             * This function has limited capabilities if the config macro FT_CONFIG_OPTION_POSTSCRIPT_NAMES is not defined in ftoption.h:
+             * It then works only for fonts that actually embed glyph names (which many recent OpenType fonts do not).
+             *
+             * This flag is defined on the MacOS distribution of nest-libs. It should be checked on other versions.
+             */
+            if (FT_Get_Glyph_Name(face, glyph_index, buf, 64)) {
+                std::cerr << "Problem getting glyph name for index_entry " << glyph_index << "\n";
+                continue;
+            }
+            std::string name(buf);
+            // std::cout << "Found name \"" << name << "\" for index_entry " << glyph_index << "\n";
+            
+            index_entry.name_begin = strings.size();
+            tex_index_entry.name_begin = strings.size();
+            strings.insert(strings.end(), name.begin(), name.end());
+            index_entry.name_end = strings.size();
+            tex_index_entry.name_end = strings.size();
+        }
+        
         /*
-         * FT_LOAD_RENDER ensures the bitmap is filled.
-         * There are other ways to do the drawing using a callback system, but as far as I can tell that only works
-         * on outlines. This approach relies on FreeType's shipped renderer and should work for everything.
+         * FT_LOAD_RENDER ensures the bitmap is filled. The default is using FT_RENDER_MODE_NORMAL,
+         * which means every byte is a pixel's alpha value.
          *
-         * I make sure to load monochrome because I don't have a great way to convert grayness into triangles.
-         * So, I should used the highest-contrast setting instead. I can always make the shape bigger for more accuracy.
+         * There are other ways to do the drawing using a callback system, but as far as I can tell that only works
+         * on outlines. This approach relies on FreeType's shipped renderer and should work for all fonts.
          */
-        if (FT_Load_Glyph(face, glyph_index, FT_LOAD_RENDER | FT_LOAD_TARGET_MONO)) {
-            std::cerr << "Warning: problem getting glyph with index " << glyph_index << " (out of "
+        if (FT_Load_Glyph(face, glyph_index, FT_LOAD_RENDER)) {
+            std::cerr << "Warning: problem getting glyph with index_entry " << glyph_index << " (out of "
                       << face->num_glyphs
                       << "glyphs)\n";
         } else {
             glyph_count++;
         }
         
-        /*
-         * There's probably not an official limit anywhere since anyone can make a font, but
-         * - https://github.com/adobe-type-tools/agl-specification
-         * - https://learn.microsoft.com/en-us/typography/opentype/spec/post
-         * - http://adobe-type-tools.github.io/afdko/OpenTypeFeatureFileSpecification.html#2fi-glyph-name
-         * agree that it's maximum 63 characters. So, 64 for the 63 and a null terminator.
-         */
-        char buf[64];
-        /*
-         * From the FreeType documentation https://freetype.org/freetype2/docs/reference/ft2-information_retrieval.html#ft_get_glyph_name:
-         *
-         * This function has limited capabilities if the config macro FT_CONFIG_OPTION_POSTSCRIPT_NAMES is not defined in ftoption.h:
-         * It then works only for fonts that actually embed glyph names (which many recent OpenType fonts do not).
-         *
-         * This flag is defined on the MacOS distribution of nest-libs. It should be checked on other versions.
-         */
-        if (FT_Get_Glyph_Name(face, glyph_index, buf, 64)) {
-            assert(false && "Problem getting glyph name");
+        { // Make the rectangle
+            index_entry.vertex_begin = vertices.size();
+            // all of these are in pixels
+            FT_Int top = face->glyph->bitmap_top;
+            FT_Int left = face->glyph->bitmap_left;
+            FT_Int bottom = top - (FT_Int) face->glyph->bitmap.rows;
+            FT_Int right = left + (FT_Int) face->glyph->bitmap.width;
+            
+            // It's a little weird that I'm going top then bottom but it helps with the pixel loop later
+            
+            // draw the first triangle
+            vertices.emplace_back(glm::vec3((float) left / PIXEL_SCALE,
+                                            (float) top / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(0.0, 0.0));
+            vertices.emplace_back(glm::vec3((float) right / PIXEL_SCALE,
+                                            (float) top / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(1.0, 0.0));
+            vertices.emplace_back(glm::vec3((float) right / PIXEL_SCALE,
+                                            (float) bottom / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(1.0, 1.0));
+            vertices.emplace_back(glm::vec3((float) left / PIXEL_SCALE,
+                                            (float) top / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(0.0, 0.0));
+            vertices.emplace_back(glm::vec3((float) right / PIXEL_SCALE,
+                                            (float) bottom / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(1.0, 1.0));
+            vertices.emplace_back(glm::vec3((float) left / PIXEL_SCALE,
+                                            (float) bottom / PIXEL_SCALE,
+                                            0),
+                                  glm::vec2(0.0, 1.0));
+            index_entry.vertex_end = vertices.size();
         }
-        std::string name(buf);
-        std::cout << "Found name \"" << name << "\" for index " << glyph_index << "\n";
         
-        uint32_t name_begin = strings.size();
-        strings.insert(strings.end(), name.begin(), name.end());
-        uint32_t name_end = strings.size();
-        
-        uint32_t vertex_begin = data.size();
-        // loop through every pixel
-        {
-            int r = 0;
+        { // Fill the texture
             FT_Bitmap &bitmap = face->glyph->bitmap;
-            for (unsigned char *row = bitmap.buffer; r < bitmap.rows; row = &row[bitmap.pitch], r++) {
-                // remember, it was rendered in mono so every pixel is one bit
-                for (int c = 0; c < bitmap.width; c++) {
-                    //   the byte's...    nth bit
-                    if ((row[c / 8] >> (c % 8)) & 1) {
-                        // draw the first triangle
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE,
-                                                    0));
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE,
-                                                    0));
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    0));
-                        // draw the second triangle
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE,
-                                                    0));
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    0));
-                        data.emplace_back(glm::vec3((float) (face->glyph->bitmap_top + r) / PIXEL_SCALE,
-                                                    (float) (face->glyph->bitmap_left + c) / PIXEL_SCALE + PIXEL_SCALE,
-                                                    0));
-                    }
+            tex_index_entry.height = bitmap.rows;
+            tex_index_entry.width = bitmap.width;
+            
+            tex_index_entry.tex_begin = texture_colors.size();
+            int r = 0;
+            // filled in row by row, top-down
+            for (uint8_t *row = bitmap.buffer; r < bitmap.rows; row = &row[bitmap.pitch], r++) {
+                int c = 0;
+                for (uint8_t *col = row; c < bitmap.width; col++, c++) {
+                    texture_colors.push_back(*col);
                 }
             }
+            
+            tex_index_entry.tex_end = texture_colors.size();
         }
-        uint32_t vertex_end = data.size();
         
-        index.push_back({name_begin, name_end, vertex_begin, vertex_end});
+        texture_indices.push_back(tex_index_entry);
+        vertex_indices.push_back(index_entry);
     }
     
-    std::cout << "Number of vertices is " << data.size() << "\n";
+    { // write rectangles to .pnct file
+        std::ofstream out(data_path("dist/Inknut_Antiqua.pnct"), std::ios::binary);
+        write_chunk("pnct", vertices, &out);
+        write_chunk("str0", strings, &out);
+        write_chunk("idx0", vertex_indices, &out);
+    }
     
-    std::ofstream out(data_path("dist/Inknut_Antiqua.pnct"), std::ios::binary);
-    write_chunk("pnct", data, &out);
-    write_chunk("str0", strings, &out);
-    write_chunk("idx0", index, &out);
+    { // write textures to texture file
+        std::ofstream out(data_path("dist/Inknut_Antiqua.txtr"), std::ios::binary);
+        write_chunk("txtr", texture_colors, &out);
+        write_chunk("str0", strings, &out);
+        write_chunk("idx1", texture_indices, &out);
+    }
     
-    std::cout << glyph_count << " glyphs recognized for index under " << face->num_glyphs << "\n";
+    std::cout << glyph_count << " glyphs recognized for vertex_indices under " << face->num_glyphs << "\n";
     
     if (FT_Done_Face(face)) {
         assert(false && "Problem destroying face");
